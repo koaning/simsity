31ca0384acd602cf7f4b43f7b22dfd5ca5271596
Vincent <vincentwarmerdam@gmail.com>
2021-10-16 22:01:36 +0200

    ja

--- simsity/datasets.py
+++ simsity/datasets.py
@@ -11,13 +11,6 @@ def fetch_clinc():
     from simsity.datasets import fetch_clinc
 
     fetch_clinc()
-
-    #                                          text
-    # 0                              find my wallet
-    # 1  can you give me the gps location of harvey
-    # 2    where's my buddy steve right this second
-    # 3        locate jenny at her present position
-    # 4          let me know where jim is right now
     ```
     """
     return pd.read_csv(
@@ -35,13 +28,6 @@ def fetch_voters():
     from simsity.datasets import fetch_voters
 
     fetch_voters()
-
-    #                      name         suburb postcode
-    # 0          khimerc thomas      charlotte    2826g
-    # 1       lucille richardst     kannapolis    28o81
-    # 2      reb3cca bauerboand        raleigh    27615
-    # 3          maleda mccloud      goldsboro    2753o
-    # 4          belida stovall     morrisvill    27560
     ```
     """
     return pd.read_csv(
