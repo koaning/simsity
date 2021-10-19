5a200c62d0a926026e2c57f9a47b3838523408bd
Vincent <vincentwarmerdam@gmail.com>
2021-10-19 23:09:52 +0200

    docs-update

--- tests/test_docs.py
+++ tests/test_docs.py
@@ -1,26 +1,8 @@
 import pytest
 import pathlib
-from mktestdocs import check_docstring, get_codeblock_members, grab_code_blocks
+from mktestdocs import check_docstring, grab_code_blocks
 
 from simsity.datasets import fetch_clinc, fetch_voters
-from simsity.service import Service
-from simsity.indexer import PyNNDescentIndexer
-
-
-@pytest.mark.parametrize(
-    "obj", get_codeblock_members(Service), ids=lambda d: d.__qualname__
-)
-def test_service_members(obj):
-    """Test methods of the `Service` class."""
-    check_docstring(obj)
-
-
-@pytest.mark.parametrize(
-    "obj", get_codeblock_members(PyNNDescentIndexer), ids=lambda d: d.__qualname__
-)
-def test_indexer_members(obj):
-    """Test methods of the `Indexer` class."""
-    check_docstring(obj)
 
 
 @pytest.mark.parametrize("func", [fetch_clinc, fetch_voters], ids=lambda d: d.__name__)
@@ -29,6 +11,7 @@ def test_function_docstrings(func):
     check_docstring(obj=func)
 
 
-def test_readme_file():
-    """Test the README file."""
-    grab_code_blocks(pathlib.Path("README.md").read_text())
+@pytest.mark.parametrize("fpath", ["README.md", "docs/quickstart/index.md"])
+def test_quickstart_docs_file(fpath):
+    """Test the quickstart files."""
+    grab_code_blocks(pathlib.Path(fpath).read_text())
