fde2406bcac76a5715f15e8541bb0137dab17332
Vincent <vincentwarmerdam@gmail.com>
2021-11-18 21:37:46 +0100

    added-multi-app

--- simsity/serve.py
+++ simsity/serve.py
@@ -28,3 +28,55 @@ def create_app(service: Service):
         return service.query(**params.query, n_neighbors=params.n_neighbors)
 
     return app
+
+
+class MultiParams(BaseModel):
+    """Parameters for the query endpoint."""
+
+    query: dict
+    n_neighbors: int = 5
+    service: str
+
+    @validator("n_neighbors")
+    def n_neighbors_must_be_positive(cls, value):
+        """Gotta make sure they're positive."""
+        if value <= 0:
+            raise ValueError(f"we expect n_neighbors >= 0, we received {value}")
+        return value
+
+
+def create_multi_app(**services):
+    """
+    Creates a FastAPI app from multiple services.
+
+    Arguments:
+        services: (name, service) pairs
+    """
+    app = FastAPI()
+
+    @app.post("/query")
+    def query(params: MultiParams):
+        """Send a query to one of multiple services"""
+        service = services[params.service]
+        return service.query(**params.query, n_neighbors=params.n_neighbors)
+
+    return app
+
+
+def run_multi_service(host, port=8080, **services):
+    """
+    Runs a single endpoint that hosts multiple services.
+
+    Once the server is started, you can `POST` the service using the following URL:
+
+    ```
+    http://<host>:<port>/query
+    ```
+
+    Arguments:
+        host: Host to bind the server to.
+        port: Port to bind the server to.
+    """
+    import uvicorn
+
+    uvicorn.run(create_multi_app(**services), host=host, port=port)
--- tests/test_web.py
+++ tests/test_web.py
@@ -1,6 +1,6 @@
 import pytest
 
-from simsity.serve import create_app
+from simsity.serve import create_app, create_multi_app
 from starlette.testclient import TestClient
 
 
@@ -16,6 +16,35 @@ def test_basic_post_rqs(clinc_service, payload, exp):
     """Confirm that the basic queries come back right"""
     client = TestClient(create_app(clinc_service))
     resp = client.post("/query", json=payload)
+    print(resp.json())
+    assert resp.status_code == 200
+    assert len(resp.json()) == payload["n_neighbors"]
+    assert any(exp in s["item"]["text"] for s in resp.json())
+
+
+@pytest.mark.parametrize(
+    "payload, exp",
+    [
+        (
+            {"query": {"text": "hello there"}, "n_neighbors": 20, "service": "clinc1"},
+            "hello",
+        ),
+        (
+            {"query": {"text": "hello there"}, "n_neighbors": 10, "service": "clinc2"},
+            "hello",
+        ),
+        (
+            {"query": {"text": "thanks"}, "n_neighbors": 10, "service": "clinc1"},
+            "thanks",
+        ),
+    ],
+)
+def test_basic_post_multi_server(clinc_service, payload, exp):
+    """Confirm that the basic queries come back right"""
+    app = create_multi_app(clinc1=clinc_service, clinc2=clinc_service)
+    client = TestClient(app)
+    resp = client.post("/query", json=payload)
+    print(resp.text)
     print(resp.json())
     assert resp.status_code == 200
     assert len(resp.json()) == payload["n_neighbors"]
