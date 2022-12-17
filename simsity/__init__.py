8e32668154fe54420a86eb8eb99c3a6481160ca3
vincent d warmerdam <vincentwarmerdam@gmail.com>
2022-12-17 16:57:19 +0000

    iter

--- simsity/__init__.py
+++
@@ -1,8 +0,0 @@
-try:
-    from importlib import metadata
-except ImportError:  # for Python<3.8
-    import importlib_metadata as metadata
-
-
-__title__ = __name__
-__version__ = metadata.version(__title__)
