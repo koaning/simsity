1752b041ce2b34fa88f97a3257d3e6368ba4ba89
vincent d warmerdam <vincentwarmerdam@gmail.com>
2022-12-17 16:43:59 +0000

    iter

--- scripts/demo-annoy.py
+++ scripts/demo-annoy.py
@@ -8,7 +8,6 @@ import pandas as pd
 
 from simsity.service import Service
 from simsity.indexer import AnnoyIndexer
-from simsity.preprocessing import ColumnGrabber
 from sklearn.pipeline import make_pipeline
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.decomposition import TruncatedSVD
@@ -18,7 +17,7 @@ df = pd.read_csv("tests/data/clinc-data.csv").head(100)
 
 # The encoder defines how we encode the data going in.
 encoder = make_pipeline(CountVectorizer(), TruncatedSVD())
-encoder.fit(df['text'].tolist())
+encoder.fit(df["text"].tolist())
 
 # The indexer handles the nearest neighbor lookup.
 indexer = AnnoyIndexer()
@@ -27,7 +26,7 @@ indexer = AnnoyIndexer()
 service = Service(indexer=indexer, encoder=encoder)
 
 # We can now build the service using this data.
-service.index(df['text'].tolist())
+service.index(df["text"].tolist())
 
 # And use it
 idx, dists = service.query(df.iloc[2], n_neighbors=3)
