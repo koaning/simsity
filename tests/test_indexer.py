bdf7b6418c2d1fa9ac303b19a1a94102a161dce2
vincent d warmerdam <vincentwarmerdam@gmail.com>
2022-12-20 12:07:04 +0000

    testfix

--- tests/test_indexer.py
+++ tests/test_indexer.py
@@ -1,4 +1,6 @@
 import pytest
+import pandas as pd
+from sklearn.pipeline import make_pipeline
 from sklearn.feature_extraction.text import CountVectorizer
 
 from simsity.indexer import AnnoyIndexer, PyNNDescentIndexer
@@ -7,12 +9,19 @@ from simsity.indexer import AnnoyIndexer, PyNNDescentIndexer
 @pytest.mark.parametrize("indexer", [AnnoyIndexer(), PyNNDescentIndexer()])
 def test_basics(indexer):
     """Test the basic API of each indexer"""
-    texts = ["this is text", "bla bla bla"]
-    cv = CountVectorizer()
-    X = cv.fit_transform(texts).toarray()
-    print(X)
+    df_clinc = pd.read_csv("tests/data/clinc-data.csv")
+    encoder = make_pipeline(CountVectorizer())
+    # We're casting to an array to make it non-sparse
+    # Not every indexer supports sparse arrays
+    X = encoder.fit_transform(df_clinc["text"]).toarray()
     indexer.index(X)
-    q = cv.transform("bla").toarray()[0]
+    q = encoder.transform("translate in english").toarray()[0]
+    
+    print(X)
     print(q)
+    
     idx, _ = indexer.query(q, n_neighbors=1)
-    assert idx == 1
+    assert len(idx) == 1
+
+    idx, _ = indexer.query(q, n_neighbors=10)
+    assert len(idx) == 1
