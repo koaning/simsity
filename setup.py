6242107b4b84c78431083db6ed979b600f45c18f
Vincent <vincentwarmerdam@gmail.com>
2022-12-20 15:16:14 +0100

    version

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ dev_packages = all_packages + docs_packages + test_packages
 
 setup(
     name="simsity",
-    version="0.4.0",
+    version="0.3.0",
     author="Vincent D. Warmerdam",
     packages=find_packages(exclude=["notebooks", "docs"]),
     description="Simple Similarity Service",
