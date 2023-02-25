2b5b7f54db028cfd490a40131dbc2b48bd1bbcb5
vincent d warmerdam <vincentwarmerdam@gmail.com>
2023-02-25 10:40:59 +0000

    gogo

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ dev_packages = all_packages + docs_packages + test_packages + nms_packages
 
 setup(
     name="simsity",
-    version="0.3.0",
+    version="0.4.0",
     author="Vincent D. Warmerdam",
     packages=find_packages(exclude=["notebooks", "docs"]),
     description="Simple Similarity Service",
