4fc0726c1c2136c35f397345770970a821a13b3c
vincent d warmerdam <vincentwarmerdam@gmail.com>
2023-03-02 19:55:41 +0000

    add-dim

--- simsity/__init__.py
+++ simsity/__init__.py
@@ -3,6 +3,7 @@ from tinydb import TinyDB
 from BetterJSONStorage import BetterJSONStorage
 
 from pathlib import Path 
+import itertools as it
 from typing import Iterable, Callable, Protocol, Union, Protocol
 
 class Transformer(Protocol):
@@ -15,10 +16,10 @@ def encode(encoder, item):
     return encoder(item)
 
 
-def create_index(data: Iterable, encoder: Union[Callable, Transformer], path: Path, space="cosine"):
-    first_item = next(data)
-    encoded = encode(encoder, first_item)
-    index = Index(space = space, dim = encoded.shape[0])
+def create_index(data: Iterable, encoder: Union[Callable, Transformer], path: Path, dim:int, space="cosine"):
+    batch = it.islice(data, 1)
+    encoded = encode(encoder, batch)
+    index = Index(space = space, dim = dim)
     index.init_index(max_elements = 10_000)
     with TinyDB(path, access_mode="r+", storage=BetterJSONStorage) as db:
         for i, item in enumerate(data):
