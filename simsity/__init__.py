cc3118b062bdc113754a2392c881afbe45c72e1d
vincent d warmerdam <vincentwarmerdam@gmail.com>
2023-03-03 02:58:09 +0000

    milestone

--- simsity/__init__.py
+++ simsity/__init__.py
@@ -1,33 +1,64 @@
+import datetime as dt
+import srsly
 from hnswlib import Index
-from tinydb import TinyDB 
-from BetterJSONStorage import BetterJSONStorage
 
 from pathlib import Path 
-import itertools as it
-from typing import Iterable, Callable, Protocol, Union, Protocol
+from typing import Iterable, Protocol
 
 class Transformer(Protocol):
     def transform(self):
         pass
 
-def encode(encoder, item):
-    if hasattr(encoder, "transform"):
-        return encoder.transform([item])[0]
-    return encoder(item)
+class SimSityIndex:
+    def __init__(self, path: Path, encoder: Transformer) -> None:
+        self.path = Path(path)
+        self.metadata = srsly.read_json(self.path / "metadata.json")
+        self.index = Index(space = self.metadata['space'], dim = self.metadata['dim'])
+        self.index.load_index(str(self.path / "index.bin"))
+        self.db = {i: k['data'] for i, k in enumerate(srsly.read_jsonl(self.path / "db.jsonl"))}
+        self.encoder = encoder
+    
+    def query(self, query, n=10):
+        arr = self.encoder.transform(query)
+        labels, distances = self.index.knn_query(arr, k=n)
+        out = [self.db[int(label)] for label in labels[0]]
+        return out, list(distances[0])
 
-
-def create_index(data: Iterable, encoder: Union[Callable, Transformer], path: Path, dim:int, space="cosine"):
-    batch = it.islice(data, 1)
-    encoded = encode(encoder, batch)
+def create_index(data: Iterable, encoder: Transformer, path: Path, dim:int, space="cosine"):
+    path = Path(path)
     index = Index(space = space, dim = dim)
-    index.init_index(max_elements = 10_000)
-    with TinyDB(path, access_mode="r+", storage=BetterJSONStorage) as db:
-        for i, item in enumerate(data):
-            db.insert({'i': i, 'data': item})
-            
+    index.init_index(max_elements = len(data))
+    encoded = encoder.transform(data)
+    index.add_items(encoded)
+    if (path / "db.jsonl").exists():
+        (path / "db.jsonl").unlink()
+    srsly.write_jsonl(path / "db.jsonl", ({'data': item} for i, item in enumerate(data)))
+    index.save_index(str(path / "index.bin"))
+    srsly.write_json(path / "metadata.json", {
+        "created": str(dt.datetime.now())[:19],
+        "dim": dim,
+        "space": space
+    })
+    return SimSityIndex(path=path, encoder=encoder)
+
+def load_index(path, encoder):
+    return SimSityIndex(path=path, encoder=encoder)
 
 
-def load_index():
-    pass
 
-class Index:
+from simsity.datasets import fetch_recipes
+
+# Fetch data
+df_recipes = fetch_recipes()
+recipes = df_recipes['text']
+
+# Create an indexer and encoder
+from embetter.text import SentenceEncoder
+encoder = SentenceEncoder()
+
+dim = encoder.transform(["yo"]).shape[1]
+index = create_index(recipes, encoder, "demoo", dim=dim)
+    
+index = load_index("demoo", encoder=encoder)
+index.query("pork")
+index.index.element_count
